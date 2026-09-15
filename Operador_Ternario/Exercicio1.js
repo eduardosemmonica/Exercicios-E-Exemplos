8035e1f9a7cde597a0c4e2a6d421e1406fcca7ea
Eduardo Emanuel Machado <eduardoemanul44@gmail.com>
2026-09-14 21:35:16 -0300

    Delete Operador_Ternario directory

--- Operador_Ternario/Exercicio1.js
+++
@@ -1,6 +0,0 @@
-//Exercício 1: Faça um programa que leia um número e informe se ele é positivo ou negativo usando o operador ternário.
-const prompt = require('prompt-sync')();
-
-let numero = Number(prompt("Digite um número: "));
-
-console.log(numero >= 0 ? "Positivo" : "Negativo");
