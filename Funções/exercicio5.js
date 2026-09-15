25394044f8f5a513df0385ad938780aedd759899
Eduardo Emanuel Machado <eduardoemanul44@gmail.com>
2026-09-14 21:34:58 -0300

    Delete Funções directory

--- Funções/exercicio5.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-
-function multiplicar(num1, num2) {
-    console.log(`O resultado da multiplicação é: ${num1 * num2}`);
-}
-let num1 = parseFloat(prompt("Digite o primeiro número: "));
-let num2 = parseFloat(prompt("Digite o segundo número: "));
-multiplicar(num1, num2);
--- Funções/funcoes_exemplo1.js
+++
@@ -1,9 +0,0 @@
-function saudacao(nome) {
-    console.log(`Bom Dia, Cliente!`); // Função sem parâmetro
-
-}
-
-saudacao(); 
-saudacao(); // pode chamar a função 
-            // quantas vezes quiser
-
